57b39626d92c52061fe9c81be0099fcf52e65965
BubblingSODA <go2gym365@gmail.com>
2023-08-08 11:18:46 +0000

    .

--- FindGym/FindGym/urls.py
+++ FindGym/FindGym/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('FindAndGoToGym.urls')),
+    path('', include('gyms.urls')),
     path('users/', include('users.urls')),
-    path("admin/", admin.site.urls)
+    path("admin/", admin.site.urls) 
 ]
